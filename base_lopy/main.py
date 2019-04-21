2879169a735b4f38d70f151be9d2c1b837c75c4e
Ioan Hedes <ihedes13@gmail.com>
2019-04-21 19:50:30 +0300

    Now the data is kept in CSV after being checked. If the packet doesn't contain a certain string, it is deemed invalid and ignored

--- base_lopy/main.py
+++ base_lopy/main.py
@@ -2,6 +2,7 @@ import pycom
 from network import LoRa
 import socket
 import time
+import machine
 
 switch = True
 
@@ -23,11 +24,28 @@ except:
     pycom.heartbeat(False)
     pycom.rgbled(0x7f0000)
 
-if switch:
+if switch == True:
     print("\n----------------------------------\nEntering main loop...\n----------------------------------\n")
     pycom.heartbeat(True)
     while True:
-        data = s.recv(64)
-        if data:
-            print(data.decode('utf-8'))
-        time.sleep(7)
+        try:
+            data = s.recv(128)
+            if data:
+                data_decoded = data.decode('utf-8')
+                decoded_data = data.decode('utf-8')
+                if decoded_data[0] == '9' and decoded_data[1] == '2':
+                    print(data_decoded)
+                    with open("log.csv", "a+") as f:
+                        f.write(data_decoded + '\n')
+                else:
+                    print("Foreign data, ignoring..")
+            else:
+                print("No data..\n")
+        except:
+            print (" ! Error, rebooting...\n")
+            machine.reset()
+        time.sleep(5)
+
+if switch == False:
+    print (" ! Rebooting...\n")
+    machine.reset()
